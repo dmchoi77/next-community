bee8954b7224208321be4f2c0acbde63d3fc36b7
dmchoi <david5760@naver.com>
2022-05-28 08:06:12 +0900

    feat: add document

---
+++ pages/_document.jsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <title>개발 커뮤니티</title>
+          <meta charSet="utf-8"></meta>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Head>
+      </Html>
+    );
+  }
+}
