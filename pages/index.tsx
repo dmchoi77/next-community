ed503a76ddfc0d71f74d9ae36c14144505531f84
dmchoi <david5760@naver.com>
2022-05-28 00:38:45 +0900

    feat(currentCategory): add function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,46 @@
-import React, { useState } from "react";
-import Head from "next/head";
-import Link from "next/link";
-import { Router } from "next/router";
+import React, { FunctionComponent, useState, useEffect } from "react";
 import Header from "../src/component/List/Header";
-import CategoryList from "../src/component/List/CategoryList"
-export default function Home() {
+import CategoryList from "../src/component/List/CategoryList";
+import PostList from "../src/component/List/PostList";
+import axios from "axios";
+import { PostProps } from "../src/types/type";
 
+const Home: FunctionComponent = () => {
+  const [posts, setPosts] = useState<PostProps[]>([])
+  const [selectingPosts, setSelectingPosts] = useState<PostProps[]>([]);
 
+  const fetchPosts = async () => {
+    const postList = await axios.get("http://localhost:4000/posts")
+    setPosts(postList.data)
+    setSelectingPosts(postList.data)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  const currentCategory = (selectedCategory: any) => {
+    let selectedPosts;
+
+    if (selectedCategory === '전체글') {
+      selectedPosts = posts
+    }
+    else if (selectedCategory === '인기글') {
+      selectedPosts = posts.filter((post) => post.viewCount >= 100)
+    }
+    else {
+      selectedPosts = posts.filter((post) => post.categoryName === selectedCategory)
+    }
+    setSelectingPosts(selectedPosts)
+  }
 
   return (
     <>
       <Header />
-      <CategoryList />
+      <CategoryList currentCategory={currentCategory} />
+      <PostList posts={selectingPosts} />
     </>
   );
 }
+
+export default Home;
