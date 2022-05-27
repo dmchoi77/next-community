5e1c4db5ec215bfa7117dd81edfa3ac7bc6e7dda
dmchoi <david5760@naver.com>
2022-05-27 23:06:10 +0900

    design(global): text-decoration - none, a tag - black

---
+++ styles/style.tsx
@@ -0,0 +1,24 @@
+// GlobalStyle
+// UPDATE :
+import { css, Global, keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const globalStyles = (
+  <Global
+    styles={css`
+  html,
+  body {
+    // width: 100vh;
+  }
+
+  * {
+    text-decoration: none;
+  }
+
+  a {
+    color:black;
+  }
+
+  `}
+  />
+);
