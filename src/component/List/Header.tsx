4599c3f7286ab7c13d165964131518c6b47f9fab
dmchoi <david5760@naver.com>
2022-05-27 18:27:54 +0900

    feat(Header): add home header

---
+++ src/component/List/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+function Header() {
+  return (
+    <HeaderWrapper>
+      커뮤니티
+    </HeaderWrapper>
+  )
+}
+
+export default Header
+
+const HeaderWrapper = styled.header`
+  width: 81px;
+  height: 32px;
+  margin: 34px 22px 20px;
+  font-size: 23px;
+  font-weight: bold;
+`
+
